ec46108b9c570f25f3960b290fbc0ff496335f53
唐干宵 <1587900660@qq.com>
2024-03-12 23:58:08 +0800

    测试环境变量

---
+++ app.config.ts
@@ -0,0 +1,13 @@
+// 在vue 组件中访问这里的全局配置：
+// const appConfig = useAppConfig()
+// console.log('appConfig :>> ', appConfig)
+
+export default defineAppConfig({
+  title: 'Hello Nuxt',
+  theme: {
+    dark: true,
+    colors: {
+      primary: '#ff0000'
+    }
+  }
+})
--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <NuxtPage />
   </div>
 </template>
+
+<script setup lang="ts">
+const a = import.meta.env.VITE_APP_NAME
+console.log('a :>> ', a)
+console.log('import.meta :>> ', import.meta)
+</script>

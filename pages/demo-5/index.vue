caaeef40472f0de6e2457735269a77863e30668e
tangganxiao <1587900660@qq.com>
2024-08-27 21:42:06 +0800

    完成等比缩放的盒子

--- pages/demo-5/index.vue
+++ pages/demo-5/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <AppHeader>等比缩放的盒子，适应不同宽度的设备，支持服务端渲染</AppHeader>
+    <AppHeader
+      :title="'等比缩放的盒子，适应不同宽度的设备，支持服务端渲染'"
+      :desc="'使用 等比缩放的盒子 + 媒体查询 + css变量实现'">
+    </AppHeader>
+
     <div class="m-auto max-w-[1400px] p-5">
       <div class="items flex flex-wrap gap-3 bg-pink-200">
         <RatioBox
@@ -10,10 +14,7 @@
             width: `calc(100% / var(--w) - var(--gap)*(var(--w) - 1)/var(--w))`,
           }">
           <div class="h-full w-full overflow-hidden">
-            <img
-              class="h-full w-full object-cover"
-              src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
-              alt="" />
+            <img class="h-full w-full object-cover" src="./test.jpeg" alt="" />
           </div>
         </RatioBox>
       </div>

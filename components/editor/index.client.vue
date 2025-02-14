b0c6702688fbe1618a9540bf3eb6a4a8a3a1a8ed
tangganxiao <tangganxiao@makeblock.com>
2025-02-14 11:51:32 +0800

    feat: 使用base64形式存储图片，无需服务器

--- components/editor/index.client.vue
+++ components/editor/index.client.vue
@@ -26,6 +26,7 @@ import {
   AutoImage,
   AutoLink,
   Autosave,
+  Base64UploadAdapter, // 将图片转换为base64
   BlockQuote,
   Bold,
   Bookmark,
@@ -49,6 +50,7 @@ import {
   ImageCaption,
   ImageEditing,
   ImageInline,
+  ImageInsert,
   ImageInsertViaUrl,
   ImageResize,
   ImageStyle,
@@ -98,10 +100,10 @@ import {
 import 'ckeditor5/ckeditor5.css';
 import translations from 'ckeditor5/translations/zh-cn.js';
 
-const LICENSE_KEY =
-  'eyJhbGciOiJFUzI1NiJ9.eyJleHAiOjE3NzEwMjcxOTksImp0aSI6IjQ5MTEzYzVkLWIyODUtNGU1Ni05YTAxLTY1NDE1MmJkZTAzYiIsInVzYWdlRW5kcG9pbnQiOiJodHRwczovL3Byb3h5LWV2ZW50LmNrZWRpdG9yLmNvbSIsImRpc3RyaWJ1dGlvbkNoYW5uZWwiOlsiY2xvdWQiLCJkcnVwYWwiXSwiZmVhdHVyZXMiOlsiRFJVUCJdLCJ2YyI6Ijc4NzRjMTJiIn0.QDDo0bmUg_MVPXC-WzmG5H60Dq62OwAsy6HdrxRCXSpCWQZxog35YwetEHSn2GCZtSfgSCjlxKrDjPL7HlvGMg';
 // const LICENSE_KEY =
-//   'eyJhbGciOiJFUzI1NiJ9.eyJleHAiOjE3NzEwMjcxOTksImp0aSI6Ijc4N2M4ZDc1LTc0NjYtNDU1Mi1iZDdlLTUyZjcwMmNhNGEwOSIsImxpY2Vuc2VkSG9zdHMiOlsiMTI3LjAuMC4xIiwibG9jYWxob3N0IiwiMTkyLjE2OC4qLioiLCIxMC4qLiouKiIsIjE3Mi4qLiouKiIsIioudGVzdCIsIioubG9jYWxob3N0IiwiKi5sb2NhbCJdLCJ1c2FnZUVuZHBvaW50IjoiaHR0cHM6Ly9wcm94eS1ldmVudC5ja2VkaXRvci5jb20iLCJkaXN0cmlidXRpb25DaGFubmVsIjpbImNsb3VkIiwiZHJ1cGFsIl0sImxpY2Vuc2VUeXBlIjoiZGV2ZWxvcG1lbnQiLCJmZWF0dXJlcyI6WyJEUlVQIl0sInZjIjoiMDk1ZTU5ZTAifQ.EwOlQV7Pj8kRAcxjYbVSJ1ieu3n3Heizlu9A4kPVTwKR0prCFc-nwrNjF9vHQAVCnJwiuUR4yxHY9vE0D1BR8Q';
+//   'eyJhbGciOiJFUzI1NiJ9.eyJleHAiOjE3NzEwMjcxOTksImp0aSI6IjQ5MTEzYzVkLWIyODUtNGU1Ni05YTAxLTY1NDE1MmJkZTAzYiIsInVzYWdlRW5kcG9pbnQiOiJodHRwczovL3Byb3h5LWV2ZW50LmNrZWRpdG9yLmNvbSIsImRpc3RyaWJ1dGlvbkNoYW5uZWwiOlsiY2xvdWQiLCJkcnVwYWwiXSwiZmVhdHVyZXMiOlsiRFJVUCJdLCJ2YyI6Ijc4NzRjMTJiIn0.QDDo0bmUg_MVPXC-WzmG5H60Dq62OwAsy6HdrxRCXSpCWQZxog35YwetEHSn2GCZtSfgSCjlxKrDjPL7HlvGMg';
+const LICENSE_KEY =
+  'eyJhbGciOiJFUzI1NiJ9.eyJleHAiOjE3NzEwMjcxOTksImp0aSI6Ijc4N2M4ZDc1LTc0NjYtNDU1Mi1iZDdlLTUyZjcwMmNhNGEwOSIsImxpY2Vuc2VkSG9zdHMiOlsiMTI3LjAuMC4xIiwibG9jYWxob3N0IiwiMTkyLjE2OC4qLioiLCIxMC4qLiouKiIsIjE3Mi4qLiouKiIsIioudGVzdCIsIioubG9jYWxob3N0IiwiKi5sb2NhbCJdLCJ1c2FnZUVuZHBvaW50IjoiaHR0cHM6Ly9wcm94eS1ldmVudC5ja2VkaXRvci5jb20iLCJkaXN0cmlidXRpb25DaGFubmVsIjpbImNsb3VkIiwiZHJ1cGFsIl0sImxpY2Vuc2VUeXBlIjoiZGV2ZWxvcG1lbnQiLCJmZWF0dXJlcyI6WyJEUlVQIl0sInZjIjoiMDk1ZTU5ZTAifQ.EwOlQV7Pj8kRAcxjYbVSJ1ieu3n3Heizlu9A4kPVTwKR0prCFc-nwrNjF9vHQAVCnJwiuUR4yxHY9vE0D1BR8Q';
 
 const editorWordCount = useTemplateRef('editorWordCountElement');
 
@@ -143,6 +145,7 @@ const config = computed(() => {
         'pageBreak',
         'link',
         'bookmark',
+        'insertImage',
         'mediaEmbed',
         'insertTable',
         'highlight',
@@ -166,6 +169,7 @@ const config = computed(() => {
       AutoImage,
       AutoLink,
       Autosave,
+      Base64UploadAdapter,
       BlockQuote,
       Bold,
       Bookmark,
@@ -188,6 +192,7 @@ const config = computed(() => {
       ImageCaption,
       ImageEditing,
       ImageInline,
+      ImageInsert,
       ImageInsertViaUrl,
       ImageResize,
       ImageStyle,
@@ -308,10 +313,10 @@ const config = computed(() => {
         'resizeImage',
       ],
     },
-    initialData: '',
-    // '<h2>Congratulations on setting up CKEditor 5! 🎉</h2>\n<p>\n\tYou\'ve successfully created a CKEditor 5 project. This powerful text editor\n\twill enhance your application, enabling rich text editing capabilities that\n\tare customizable and easy to use.\n</p>\n<h3>What\'s next?</h3>\n<ol>\n\t<li>\n\t\t<strong>Integrate into your app</strong>: time to bring the editing into\n\t\tyour application. Take the code you created and add to your application.\n\t</li>\n\t<li>\n\t\t<strong>Explore features:</strong> Experiment with different plugins and\n\t\ttoolbar options to discover what works best for your needs.\n\t</li>\n\t<li>\n\t\t<strong>Customize your editor:</strong> Tailor the editor\'s\n\t\tconfiguration to match your application\'s style and requirements. Or\n\t\teven write your plugin!\n\t</li>\n</ol>\n<p>\n\tKeep experimenting, and don\'t hesitate to push the boundaries of what you\n\tcan achieve with CKEditor 5. Your feedback is invaluable to us as we strive\n\tto improve and evolve. Happy editing!\n</p>\n<h3>Helpful resources</h3>\n<ul>\n\t<li>📝 <a href="https://portal.ckeditor.com/checkout?plan=free">Trial sign up</a>,</li>\n\t<li>📕 <a href="https://ckeditor.com/docs/ckeditor5/latest/installation/index.html">Documentation</a>,</li>\n\t<li>⭐️ <a href="https://github.com/ckeditor/ckeditor5">GitHub</a> (star us if you can!),</li>\n\t<li>🏠 <a href="https://ckeditor.com">CKEditor Homepage</a>,</li>\n\t<li>🧑‍💻 <a href="https://ckeditor.com/ckeditor-5/demo/">CKEditor 5 Demos</a>,</li>\n</ul>\n<h3>Need help?</h3>\n<p>\n\tSee this text, but the editor is not starting up? Check the browser\'s\n\tconsole for clues and guidance. It may be related to an incorrect license\n\tkey if you use premium features or another feature-related requirement. If\n\tyou cannot make it work, file a GitHub issue, and we will help as soon as\n\tpossible!\n</p>\n',
+    initialData:
+      '<h2>Congratulations on setting up CKEditor 5! 🎉</h2>\n<p>\n\tYou\'ve successfully created a CKEditor 5 project. This powerful text editor\n\twill enhance your application, enabling rich text editing capabilities that\n\tare customizable and easy to use.\n</p>\n<h3>What\'s next?</h3>\n<ol>\n\t<li>\n\t\t<strong>Integrate into your app</strong>: time to bring the editing into\n\t\tyour application. Take the code you created and add to your application.\n\t</li>\n\t<li>\n\t\t<strong>Explore features:</strong> Experiment with different plugins and\n\t\ttoolbar options to discover what works best for your needs.\n\t</li>\n\t<li>\n\t\t<strong>Customize your editor:</strong> Tailor the editor\'s\n\t\tconfiguration to match your application\'s style and requirements. Or\n\t\teven write your plugin!\n\t</li>\n</ol>\n<p>\n\tKeep experimenting, and don\'t hesitate to push the boundaries of what you\n\tcan achieve with CKEditor 5. Your feedback is invaluable to us as we strive\n\tto improve and evolve. Happy editing!\n</p>\n<h3>Helpful resources</h3>\n<ul>\n\t<li>📝 <a href="https://portal.ckeditor.com/checkout?plan=free">Trial sign up</a>,</li>\n\t<li>📕 <a href="https://ckeditor.com/docs/ckeditor5/latest/installation/index.html">Documentation</a>,</li>\n\t<li>⭐️ <a href="https://github.com/ckeditor/ckeditor5">GitHub</a> (star us if you can!),</li>\n\t<li>🏠 <a href="https://ckeditor.com">CKEditor Homepage</a>,</li>\n\t<li>🧑‍💻 <a href="https://ckeditor.com/ckeditor-5/demo/">CKEditor 5 Demos</a>,</li>\n</ul>\n<h3>Need help?</h3>\n<p>\n\tSee this text, but the editor is not starting up? Check the browser\'s\n\tconsole for clues and guidance. It may be related to an incorrect license\n\tkey if you use premium features or another feature-related requirement. If\n\tyou cannot make it work, file a GitHub issue, and we will help as soon as\n\tpossible!\n</p>\n',
     language: 'zh-cn',
-    licenseKey: LICENSE_KEY,
+    licenseKey: 'GPL' || LICENSE_KEY,
     link: {
       addTargetToExternalLinks: true,
       defaultProtocol: 'https://',
--- pages/demo-13/index.vue
+++ pages/demo-13/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="">
+  <div>
     <Demo13 />
   </div>
 </template>

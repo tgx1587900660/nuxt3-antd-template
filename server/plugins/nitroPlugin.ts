671a8f50781f15e79dd62e7f9d15d4d14cb3a849
tangganxiao <tangganxiao@makeblock.com>
2025-04-24 19:33:34 +0800

    在服务端获取user-Agent的值做判断

--- server/plugins/nitroPlugin.ts
+++ server/plugins/nitroPlugin.ts
@@ -1,3 +1,11 @@
 export default defineNitroPlugin((nitroApp) => {
-  // console.log('Nitro plugin:', nitroApp);
+  nitroApp.hooks.hook('render:html', (ctx, { event }) => {
+    const userAgent = event.headers.get('user-agent') || '';
+    const pattern = /123|abc|(AppleWebKit\/537\.36)/;
+    if (pattern.test(userAgent)) {
+      console.log('在 123 或 abc 或 AppleWebKit/537.36 中');
+    }
+
+    ctx.body.push(`<script>console.log('test server plugins nitroPlugin'</script>`);
+  });
 });
